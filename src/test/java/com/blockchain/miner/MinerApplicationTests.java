b33740819f202f08908110c8ae7fac238c9c824e
LancelotTheFirst <rrevyakin@gmail.com>
2020-07-11 15:28:02 +0300

    fix: existed default empty test with annotation @Test launched my command line app on tests stage while I just wanted to build a jar.

--- src/test/java/com/blockchain/miner/MinerApplicationTests.java
+++ src/test/java/com/blockchain/miner/MinerApplicationTests.java
@@ -1,13 +1,8 @@
 package com.blockchain.miner;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MinerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
